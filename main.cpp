c42779e3f73bc20572a4f84428be3035b60091a1
Francisco J. Becerra <fjbecerr2@gmail.com>
2022-06-23 08:28:39 +0000

    Commit diario

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+using namespace std;
+/* En el caso de no especificar el namespace
+hay que incluir std::
+Ejemplo (sin namespace):
+  std::cout << "Hello World!\n";
+Ejemplo (con namespace)
+  cout << "Hello World!\n";
+*/
 
 int main() {
-  std::cout << "Hello World!\n";
+  cout<<" ---\n";
+  cout<<"[O O]\n";
+  cout<<" ---\n";
+  cout << "------------------\n";
+  cout << "FORMACIÓN C++ 2022"<<endl; 
+  cout << "------------------";
+   return 0;
 }
+
+/* TO-DO
+typedef type newname;
+
+enum color { red, green, blue } c;
+c = blue;
+enum color { red, green = 5, blue };
+*/
